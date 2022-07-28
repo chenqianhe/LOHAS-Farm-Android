3303727d1a958c7c42beff825b60f2bb3b28735b
chenqianhe <1278095698@qq.com>
2022-07-28 19:07:25 +0800

    fix: 修复BottomNavigation不显示图标

--- app/src/main/java/com/example/lohasfarm/ui/main/MainPage.kt
+++ app/src/main/java/com/example/lohasfarm/ui/main/MainPage.kt
@@ -9,6 +9,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -43,7 +44,7 @@ fun MainPage(actions: Actions) {
                             } else {
                                 painterResource(tab.icon)
                             }
-                            Icon(painter, contentDescription = null)
+                            Icon(painter, contentDescription = null,  tint= Color.Unspecified)
                         },
                         label = { Text(stringResource(tab.title).uppercase(Locale.ROOT),
                             color = if (tab == position) {
